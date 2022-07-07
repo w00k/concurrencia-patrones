8d81980dfe052f038e1b1468923748965d59395c
Francisco Arenas <francisco.arenas@globant.com>
2022-07-06 20:51:55 -0400

    singleton patern usando lock y unlock

---
+++ design_patterns/singleton.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Database struct{}
+
+func (Database) CreateSingleConnection() {
+	fmt.Println("Creating Singleton for Database")
+	time.Sleep(2 * time.Second)
+	fmt.Println("Creation Done")
+}
+
+var db *Database
+var lock sync.Mutex
+
+func getDatabaseInstance() *Database {
+	lock.Lock() //sin el bloqueo se crea cada conexión cada vez
+	defer lock.Unlock()
+	if db == nil {
+		fmt.Println("Creating DB Connection")
+		db = &Database{}
+		db.CreateSingleConnection()
+	} else {
+		fmt.Println("DB Already Created")
+	}
+	return db
+}
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			getDatabaseInstance()
+		}()
+	}
+	wg.Wait()
+}

ba48959d005ac3f1f22c085af1bcf9c39264044e
Francisco Arenas <francisco.arenas@globant.com>
2022-07-07 02:09:44 -0400

    app que permite enviar y leer data desde un host:port, para nuestro sistema de mensajería

---
+++ net/netcat.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+var (
+	port = flag.Int("p", 3090, "port")
+	host = flag.String("h", "localhost", "host")
+)
+
+// -> host:port
+// escribir -> host:port
+// -> hola host:port -> [hola]
+func main() {
+	flag.Parse()
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", *host, *port))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn)
+		done <- struct{}{}
+	}()
+	CopyContent(conn, os.Stdin)
+	conn.Close()
+	<-done
+}
+
+func CopyContent(dst io.Writer, src io.Reader) {
+	_, err := io.Copy(dst, src)
+	if err != nil {
+		log.Fatal(err)
+	}
+}

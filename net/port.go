4deeb1ff8f3339d9c4f10c4e2ea8f8aa8dc349ea
Francisco Arenas <francisco.arenas@globant.com>
2022-07-06 23:17:40 -0400

    cambio para que el scan sea a una página ingresada por parámetros usando flag, --site=<nombre de la página web>. Nota esto es solo con fines didácticos

--- net/port.go
+++ net/port.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"sync"
 )
+
+//flag para recibirlo como parámetro site
+var site = flag.String("site", "scanme.nmap.org", "URl to scan")
 
 func main() {
 	//revisar si los puertos estan libres y hacer una conexión tcp,
 	//si funciona imprimimos que el puerto esta abierto
-	for i := 0; i < 100; i++ {
-		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", i))
-		if err != nil {
-			continue
-		}
-		conn.Close()
-		fmt.Printf("Port %d is open\n", i)
+
+	flag.Parse() //permite usar *site como variable
+	var wg sync.WaitGroup
+
+	for i := 0; i < 65535; i++ {
+		wg.Add(1)
+		go func(port int) {
+			defer wg.Done()
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", *site, port))
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("Port %d is open\n", port)
+		}(i)
 	}
+	wg.Wait() //fuera del for, sino se va a bloquear uno por uno
 }
